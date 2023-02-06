1df65108fbb67dcd7448f8263f9feae8e4724059
Hakan Öcal <112850166+hknocal@users.noreply.github.com>
2023-02-06 13:44:36 +0000

    first and final commit

---
+++ fix-samples.sh
@@ -0,0 +1,7 @@
+cd samples/
+mv animals/countries/ .
+mv animals/Monkies txt animals/Monkeys.txt
+echo Monkeys > animals/Monkeys.txt
+mv movies/Gemany.txt countries/
+mv countries/.apes.txt animals/
+cd -
